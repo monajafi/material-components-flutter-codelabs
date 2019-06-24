4b1ff0507b5d80e44f63b4902712f98c8112c526
monajafi <monajafi@gmail.com>
2019-06-24 16:19:29 +0430

    xcvxc

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart';
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -23,6 +23,7 @@ class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       body: SafeArea(
         child: ListView(
@@ -31,16 +32,29 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(height: 80.0),
             Column(
               children: <Widget>[
-                Image.asset('assets/diamond.png'),
+                Image.asset("assets/diamond.png"),
                 SizedBox(height: 16.0),
-                Text('SHRINE'),
+                Text("SHRINE"),
               ],
             ),
             SizedBox(height: 120.0),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: "Username"
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: "Password"
+              ),
+              obscureText: true,
+            )
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
             // TODO: Add button bar (101)
           ],
         ),
