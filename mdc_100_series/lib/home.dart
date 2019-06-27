784d7d6193f71a8d208a3d58e7c6405dbb9b2fc7
Mousa Najafi <monajafi@gmail.com>
2019-06-27 20:21:02 +0430

    102 completed

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -12,21 +12,103 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'model/product.dart';
+import 'model/products_repository.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
-  // TODO: Make a collection of cards (102)
+  List<Card> _buildCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+    if (products == null || products.isEmpty) {
+      const <Card>[];
+    }
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        product.name,
+                        style: theme.textTheme.title,
+                        maxLines: 1,
+                      ),
+                      SizedBox(height: 8.0),
+                      Text(
+                        formatter.format(product.price),
+                        style: theme.textTheme.body2,
+                      )
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it smart guy!'),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: "menu",
+          ),
+          onPressed: () {
+            print("menu button clicked");
+          },
+        ),
+        title: Text("SHRINE"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: "search",
+            ),
+            onPressed: () {
+              print("search clicked");
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: "filter",
+            ),
+            onPressed: () {
+              print("filter clicked");
+            },
+          )
+        ],
       ),
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8 / 9,
+          children: _buildCards(context)),
       resizeToAvoidBottomInset: false,
     );
   }
--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
