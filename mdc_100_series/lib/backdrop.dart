4fbdc06e552aded3740731135c2cef5dc967a59d
monajafi <monajafi@gmail.com>
2019-07-06 16:10:09 +0430

    sdfdsfdsf

--- mdc_100_series/lib/backdrop.dart
+++ mdc_100_series/lib/backdrop.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'model/product.dart';
+
+const double _kFlingVelocity = 2.0;
 
 class Backdrop extends StatefulWidget {
   final Category currentCategory;
@@ -25,14 +28,78 @@ class Backdrop extends StatefulWidget {
   _BackdropState createState() => _BackdropState();
 }
 
+class _FrontLayer extends StatelessWidget {
+
+  const _FrontLayer({Key key,this.child}): super(key: key);
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 16.0,
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0))
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: child,
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class _BackdropState extends State<Backdrop> with SingleTickerProviderStateMixin{
   GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
-  Widget _buildStack(){
+  AnimationController _controller;
+  @override
+  void initState(){
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      value: 1.0,
+      vsync: this
+    );
+  }
+
+  bool get _frontLayerVisible{
+    final AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed || 
+           status == AnimationStatus.forward;
+  }
+
+  void toggleBackdropLayerVisibility(){
+    _controller.fling(
+      velocity: _frontLayerVisible ? -_kFlingVelocity : _kFlingVelocity
+    );
+  }
+
+  @override 
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _buildStack(BuildContext context,BoxConstraints constraints){
+    const double layerTitleHeight = 48.0;
+    final Size layerSize = constraints.biggest;
+    final double layerTop = layerSize.height - layerTitleHeight;
+    Animation<RelativeRect> layerAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(0.0, layerTop, 0.0, layerTop - layerSize.height),
+      end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0)
+      ).animate(_controller.view);
+
     return Stack(
       key: _backdropKey,
       children: <Widget>[
-        widget.backLayer,
-        widget.frontLayer
+        ExcludeSemantics(
+          child: widget.backLayer,
+          excluding: _frontLayerVisible,
+        ),
+        _FrontLayer(child: widget.frontLayer,)
       ],
     );
   }
