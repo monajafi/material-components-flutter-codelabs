897e5981c8478ecfa1d2444ca37fbe9438f3355a
monajafi <monajafi@gmail.com>
2019-07-08 07:20:11 +0430

    jhg

--- mdc_100_series/lib/backdrop.dart
+++ mdc_100_series/lib/backdrop.dart
@@ -18,31 +18,38 @@ class Backdrop extends StatefulWidget {
     @required this.frontLayer,
     @required this.backTitle,
     @required this.frontTitle,
-  }): assert(currentCategory != null),
-      assert(backLayer != null),
-      assert(frontLayer != null),
-      assert(backTitle != null),
-      assert(frontTitle != null);
+  })  : assert(currentCategory != null),
+        assert(backLayer != null),
+        assert(frontLayer != null),
+        assert(backTitle != null),
+        assert(frontTitle != null);
 
   @override
   _BackdropState createState() => _BackdropState();
 }
 
 class _FrontLayer extends StatelessWidget {
-
-  const _FrontLayer({Key key,this.child}): super(key: key);
+  const _FrontLayer({Key key, this.child, this.onTap}) : super(key: key);
   final Widget child;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     return Material(
       elevation: 16.0,
       shape: BeveledRectangleBorder(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0))
-      ),
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0))),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: onTap,
+            child: Container(
+              height: 40.0,
+              alignment: AlignmentDirectional.centerStart,
+            ),
+          ),
           Expanded(
             child: child,
           )
@@ -52,58 +59,54 @@ class _FrontLayer extends StatelessWidget {
   }
 }
 
-class _BackdropState extends State<Backdrop> with SingleTickerProviderStateMixin{
+class _BackdropState extends State<Backdrop>
+    with SingleTickerProviderStateMixin {
   GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
   AnimationController _controller;
- 
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: Duration(milliseconds: 300),
-      value: 1.0,
-      vsync: this
-    );
+        duration: Duration(milliseconds: 300), value: 1.0, vsync: this);
   }
 
-  bool get _frontLayerVisible{
+  bool get _frontLayerVisible {
     final AnimationStatus status = _controller.status;
-    return status == AnimationStatus.completed || 
-           status == AnimationStatus.forward;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
   }
 
-  void toggleBackdropLayerVisibility(){
+  void toggleBackdropLayerVisibility() {
     _controller.fling(
-      velocity: _frontLayerVisible ? -_kFlingVelocity : _kFlingVelocity
-    );
+        velocity: _frontLayerVisible ? -_kFlingVelocity : _kFlingVelocity);
   }
 
   @override
   void didUpdateWidget(Backdrop old) {
     super.didUpdateWidget(old);
-         _controller.fling(velocity: _kFlingVelocity);
-
-    // if(widget.currentCategory != old.currentCategory){
-    //   toggleBackdropLayerVisibility();
-    // }else if(!_frontLayerVisible){
-    //  _controller.fling(velocity: _kFlingVelocity);
-    // }
+    if (widget.currentCategory != old.currentCategory) {
+      toggleBackdropLayerVisibility();
+    } else if (!_frontLayerVisible) {
+      _controller.fling(velocity: _kFlingVelocity);
+    }
   }
 
-  @override 
-  void dispose(){
+  @override
+  void dispose() {
     _controller.dispose();
     super.dispose();
   }
 
-  Widget _buildStack(BuildContext context,BoxConstraints constraints){
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
     const double layerTitleHeight = 48.0;
     final Size layerSize = constraints.biggest;
     final double layerTop = layerSize.height - layerTitleHeight;
     Animation<RelativeRect> layerAnimation = RelativeRectTween(
-      begin: RelativeRect.fromLTRB(0.0, layerTop, 0.0, layerTop - layerSize.height),
-      end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0)
-      ).animate(_controller.view);
+            begin: RelativeRect.fromLTRB(
+                0.0, layerTop, 0.0, layerTop - layerSize.height),
+            end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0))
+        .animate(_controller.view);
 
     return Stack(
       key: _backdropKey,
@@ -114,46 +117,50 @@ class _BackdropState extends State<Backdrop> with SingleTickerProviderStateMixin
         ),
         PositionedTransition(
           rect: layerAnimation,
-          child: _FrontLayer(child: widget.frontLayer,) ,
+          child: _FrontLayer(
+            onTap: toggleBackdropLayerVisibility,
+            child: widget.frontLayer,
+          ),
         )
       ],
     );
   }
+
   @override
   Widget build(BuildContext context) {
     var appBar = AppBar(
-        brightness: Brightness.light,
-        elevation: 0.0,
-        titleSpacing: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: (){
-            toggleBackdropLayerVisibility();
-          },
+      brightness: Brightness.light,
+      elevation: 0.0,
+      titleSpacing: 0.0,
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: () {
+          toggleBackdropLayerVisibility();
+        },
+      ),
+      title: Text('SHRINE'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.search,
+            semanticLabel: 'search',
+          ),
+          onPressed: () {},
         ),
-        title: Text('SHRINE'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              semanticLabel: 'search',
-            ),
-            onPressed: () {
-            },
+        IconButton(
+          icon: Icon(
+            Icons.tune,
+            semanticLabel: 'filter',
           ),
-          IconButton(
-            icon: Icon(
-              Icons.tune,
-              semanticLabel: 'filter',
-            ),
-            onPressed: () {
-            },
-          ),
-        ],
-      );
+          onPressed: () {},
+        ),
+      ],
+    );
     return Scaffold(
       appBar: appBar,
-      body: LayoutBuilder(builder: _buildStack,),
+      body: LayoutBuilder(
+        builder: _buildStack,
+      ),
     );
   }
 }
